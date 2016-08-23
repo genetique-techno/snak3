9040ba491579c9dfec1518608333462bd129d77e
Aaron Todd <atodd@onetwosee.com>
2016-08-22 23:35:22 -0400

    State management Really simple API so app can know about:
    - which main pass
    - which game type
    - which overlay
    
    Certain main pass or overlay logic will use this api to set new state, which then triggers the application to execute a function b/c it's listening for new state events

---
+++ app/controllers/stateManager.js
@@ -0,0 +1,48 @@
+
+import EventEmitter from 'events';
+import gameTypes from 'app/config/gameTypes.js';
+
+class StateManager extends EventEmitter {
+
+  constructor() {
+  
+    this._state = {
+      mainPass: 'titlePass',
+      overlay: 'menu',
+      gameType: gameTypes[0]
+    };
+  }
+
+  setNewApplicationState( obj ) {
+    if ( !( obj instanceof Object ) && typeof obj['mainPass'] === 'undefined' ) {
+      return console.log( `Invalid State Transition: new MainPass requested with ${obj}` );
+    }
+
+    this._state = obj;
+    this.emit( 'newApplicationState', this._state );
+  }
+
+  setNewGameTypeState( obj ) {
+    if ( !( obj instanceof Object ) && typeof obj['gameType'] === 'undefined' ) {
+      return console.log( `Invalid State Transition: new GameType requested with ${obj}` );
+    }
+
+    this._state['gameType'] = obj['gameType'];
+    this.emit( 'newGameTypeState', this._state );
+  }
+
+  setNewOverlayState( obj ) {
+    if ( !( obj instanceof Object ) && typeof obj['overlay'] === 'undefined' ) {
+      return console.log( `Invalid State Transition: new Overlay requested with ${obj}` );
+    }
+
+    this._state['overlay'] = obj['overlay'];
+    this.emit( 'newOverlayState', this._state );
+  }
+
+  getState() {
+    return this._state;
+  }
+}
+
+export default new StateManager();
