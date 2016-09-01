33aba63454f7f6690e3d0b07a6b627e088761baf
Aaron Todd <atodd@onetwosee.com>
2016-09-01 08:27:05 -0400

    Menu removes key listener - the keylistener function was added using .bind, but removing it using .bind causes a new reference to the function to be created. - to remove, you must assign a var like `var a = func.bind( this )`, then you can add and remove the function from listener using the reference `a`

--- app/controllers/Menu.js
+++ app/controllers/Menu.js
@@ -4,6 +4,31 @@ import stateManager from 'app/controllers/stateManager.js';
 
 import menuItems from 'app/config/menuItems.js';
 
+function keyListener(e) {
+
+  let keyCode = _.result({
+    // '37': 'left',
+    '38': 'up',
+    // '39': 'right',
+    '40': 'down',
+    // '16': 'in',
+    // '17': 'out',
+    '13': 'return'
+  }, e.keyCode, null);
+  
+  switch ( keyCode ) {
+    case 'up':
+      this.decrementSelection();
+      break;
+    case 'down':
+      this.incrementSelection();
+      break;
+    case 'return':
+      this.acceptSelection();
+      break;
+  }
+}
+
 export default class Menu extends EventEmitter {
 
   constructor() {
@@ -12,34 +37,11 @@ export default class Menu extends EventEmitter {
     this.menuItems = menuItems;
     this.selectionIndex = 0;
     this.logSelection;
+    this.keyListener = keyListener.bind( this );
 
-    window.addEventListener( 'keydown', this.keyListener.bind( this ) );
+    window.addEventListener( 'keydown', this.keyListener );
   }
 
-  keyListener(e) {
-
-    let keyCode = _.result({
-      // '37': 'left',
-      '38': 'up',
-      // '39': 'right',
-      '40': 'down',
-      // '16': 'in',
-      // '17': 'out',
-      '13': 'return'
-    }, e.keyCode, null);
-    
-    switch ( keyCode ) {
-      case 'up':
-        this.decrementSelection();
-        break;
-      case 'down':
-        this.incrementSelection();
-        break;
-      case 'return':
-        this.acceptSelection();
-        break;
-    }
-  }
 
   decrementSelection() {
     this.selectionIndex = this.selectionIndex === 0 ? 0 : this.selectionIndex - 1;
@@ -64,6 +66,8 @@ export default class Menu extends EventEmitter {
     if ( item.type = 'function' ) {
       stateManager[ item.value ]( item.config );
     }
+
+    window.removeEventListener( 'keydown', this.keyListener );
 
   }
 }
