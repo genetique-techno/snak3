50fea875760fc4b70da71a8316011314c5be0a8d
Aaron Todd <atodd@onetwosee.com>
2016-09-11 17:38:20 -0400

    Fix levelling up - level up could be placed in snake nodes because I used `avoid.map` instead of `avoid = avoid.map`

--- app/controllers/Game.js
+++ app/controllers/Game.js
@@ -67,7 +67,6 @@ class Game extends EventEmitter {
     window.addEventListener( 'keydown', this.keyListener );
     let dontExtendSnake = true;
     this.levelUp( dontExtendSnake );
-    console.log(this.gameStatus);
   }
 
 
@@ -76,7 +75,7 @@ class Game extends EventEmitter {
     let avoid = this._snake.nodes;
     let rnd;
 
-    avoid.map( function( item ) {
+    avoid = avoid.map( function( item ) {
       return item.join('$');
     });
 
